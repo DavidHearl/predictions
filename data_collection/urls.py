e0a1a3d5ff72944a38df86273372fc1315a066e9
DavidHearl <david.hearl@outlook.com>
2025-06-17 22:07:05 +0100

    added very basic terrible looking styles

--- data_collection/urls.py
+++ data_collection/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('players/', views.players, name='players'),
+    path('players/<int:pk>/', views.player_detail, name='player_detail'),
+    path('players/<int:pk>/edit/', views.player_edit, name='player_edit'),
 ]
--- data_collection/views.py
+++ data_collection/views.py
@@ -1,5 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import *
+from .forms import *
+from django.core.paginator import Paginator
 
 
 def home(request):
-    return render(request, 'data_collection/home.html')
+    return render(request, "data_collection/home.html")
+
+
+def players(request):
+    query = request.GET.get("q", "")
+    players_qs = Player.objects.all()
+    if query:
+        players_qs = players_qs.filter(name__icontains=query)
+    total_count = Player.objects.count()
+    filtered_count = players_qs.count()
+    paginator = Paginator(players_qs, 200)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(
+        request,
+        "data_collection/players.html",
+        {
+            "players": page_obj.object_list,
+            "page_obj": page_obj,
+            "query": query,
+            "total_count": total_count,
+            "filtered_count": filtered_count,
+        },
+    )
+
+
+def player_detail(request, pk):
+    player = get_object_or_404(Player, pk=pk)
+    return render(request, "data_collection/player_detail.html", {"player": player})
+
+
+def player_edit(request, pk):
+    player = get_object_or_404(Player, pk=pk)
+    if request.method == "POST":
+        form = PlayerForm(request.POST, instance=player)
+        if form.is_valid():
+            form.save()
+            return redirect("player_detail", pk=player.pk)
+    else:
+        form = PlayerForm(instance=player)
+    return render(request, "data_collection/player_edit.html", {"form": form, "player": player})
